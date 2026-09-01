458df1cda383c333d6899a920fe5e3551efda3e0
Tulasi Mulugu <tulasicharymulugu082@gmail.com>
2026-09-01 13:39:26 -0400

    Initial project setup: Spring Boot 3, JPA, MySQL, Actuator, DevTools configured

---
+++ src/main/java/com/luv2code/patient_care_system/PatientCareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.patient_care_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientCareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientCareSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/patient_care_system/PatientCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.patient_care_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
